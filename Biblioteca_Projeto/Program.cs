758347d6a7775b537c65f45a4f5752777016c041
gabriel ferreira <victorgit28@outlook.com>
2024-10-10 08:41:57 -0300

    Criando classe biblioteca

--- Biblioteca_Projeto/Program.cs
+++ Biblioteca_Projeto/Program.cs
@@ -1,4 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+public class Biblioteca{
+    public List<Livro> livros;
+    public Biblioteca(){
+        livros = new List<Livro>();
+    }
+    public void Listarlivro(){
+        if(livros.Count != 0){
+            foreach(var livro in livros){
+                Console.WriteLine($"Titulo: {livro.nome} - Autor: {livro.autor} - Genero: {livro.genero} - Quantidade disponivel: {livro.quantidade} - ID: {livro.id}");
+            }
+        }else
+            Console.WriteLine("Não existe livros cadastrados!");
+    }
+}
 public class UserCliente{
     public string nome {get; set;}
     public string email {get; set;}
@@ -13,19 +28,11 @@ public class UserCliente{
         senha = s;
         idade = i;
     }
+    
 }
-public class UserAdm{
-    public string nome {get; set;}
-    public string email {get; set;}
-    public string user {get; set;}
-    public string senha {get; set;}
-    private List<Livro> livros;
-    public UserAdm(string n, string e, string u, string s){
-        nome = n;
-        email = e;
-        user = u;
-        senha = s;
-        livros = new List<Livro>();
+public class UserAdm : UserCliente{
+    public UserAdm(string n, string e, string u, string s, int i) : base(n , e , u , s, i){
+        
     }
     
     public void cadastrarLivro(){
@@ -46,7 +53,36 @@ public class UserAdm{
         Console.WriteLine("Informe o Id do livro que deseja alterar: ");
         int id = int.Parse(Console.ReadLine());
         Livro livro = livros.Find(l => l.id == id);
+        if(livro != null){
+            Console.WriteLine("Altere o titul:");
+            string n = Console.ReadLine();
+            livro.nome = n;
+            Console.WriteLine("Altere o autor:"); 
+            string a = Console.ReadLine();
+            livro.autor = a;
+            Console.WriteLine("Altere o genero:");
+            string g = Console.ReadLine();
+            livro.genero = g;
+            Console.WriteLine("Altere a quantidade: ");
+            int q = int.Parse(Console.ReadLine());
+            livro.quantidade = q;
+            Console.WriteLine("Altere o id: ");
+            int i = int.Parse(Console.ReadLine());
+            livro.id = i;
+        }else
+            Console.WriteLine("Livro não encontrado!");
     }
+    public void excluirLivro(){
+        Console.WriteLine("Informe o Id do livro que deseja excluir: ");
+        int id = int.Parse(Console.ReadLine());
+        Livro livro = livros.Find(l => l.id == id);
+        if(livro != null){
+            livros.Remove(livro);
+            Console.WriteLine("Livro removido!");
+        }else
+            Console.WriteLine("Livro não encontrado!");
+    }
+    
 }
 public class Livro{
     public string nome {get; set;}
